020f718053282228d9e50f3634a893395470c5bd
Filipe Báfica <filipe.bafica@hotmail.com>
2023-02-17 15:42:58 -0300

    wip

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -10,7 +10,7 @@ cp wp-config-sample.php wp-config.php
 
 sleep 10
 # setups 5 min installation
-wp core install --allow-root --url=https://localhost:443 --title="A very creative blog title" --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=$MYSQL_USER@email.com
+wp core install --allow-root --url=https://fbafica.42.fr --title="A very creative blog title" --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=$MYSQL_USER@email.com
 wp user create --allow-root $WORDPRESS_USER $WORDPRESS_USER@email.com --user_pass=$WORDPRESS_PASSWORD 
 
 # starts php-fpm deamon in foreground mode
